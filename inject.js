5d79cabf947fcf8d422ce1291d5b9ab677869ba0
N.R.T <nathsharwor@hotmail.com>
2017-09-10 09:25:37 +0800

    Minify

--- inject.js
+++ inject.js
@@ -1,15 +1,1 @@
-(function() {
-	function i(e) {
-		for (var n = 0, e = e.children; n < e.length; n++) {
-			if (t.indexOf(e[n].tagName.toLowerCase()) == -1)
-				t.push(e[n].tagName.toLowerCase());
-			if (e[n].childElementCount) i(e[n])
-		}
-	}
-	var t = [];
-	i(document.body);
-	t.forEach(function(t) {
-		var e = document.createElement("style");
-		e.innerHTML = t + "{font-family:'Comic Sans MS'!important}", document.head.append(e)
-	});
-}())
+(function() {function i(e){for(var n=0,e=e.children;n<e.length;n++)-1==t.indexOf(e[n].tagName.toLowerCase())&&t.push(e[n].tagName.toLowerCase()),e[n].childElementCount&&i(e[n])}var t=[];i(document.body),t.forEach(function(e){var t=document.createElement("style");t.innerHTML=e+"{font-family:'Comic Sans MS'!important}",document.head.append(t)});}())
